1b19af9957db68542c2e8c18dddd580377455df3
Vaibhav Dubewar <70598869+vaibhavd21@users.noreply.github.com>
2021-07-14 15:46:57 +0530

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.urls import path, include
 from django.views.static import serve
 from django.conf.urls import url
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
